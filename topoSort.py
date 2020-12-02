28a831ac8cc6e67b8f944d30ee69314c1260325c
4nthonyCastro <anthony.castro.tech@gmail.com>
2020-12-02 14:41:14 -0600

    Extra, Create Output File
    Creating an output file was not part of the original question.

--- topoSort.py
+++ topoSort.py
@@ -1,4 +1,9 @@
-
+# [Interview Question - December 2nd, 2020]
+#
+# Objective: 
+#     Each 'task', has a list of objectives to complete.
+#     Create a function to pass in a 'task', sort the 'task' respected objectives (values), and print the output.
+#
 
 myTask = {
     "A" : [],
@@ -16,13 +21,21 @@ def sortTask(task, myTask):
     for x in myArr:
         tempArr.extend(x)
         tempArr.sort()
+    print (tempArr)
+    return tempArr
 
-    print(tempArr)
-        
 
-sortTask('A', myTask)
-sortTask('B', myTask)
-sortTask('C', myTask)
-sortTask("D", myTask)
-sortTask("E", myTask)
-        
+outputList = sortTask('A', myTask)
+outputList.append(sortTask('B', myTask))
+outputList.append(sortTask('C', myTask))
+outputList.append(sortTask('D', myTask))
+outputList.append(sortTask('E', myTask))
+
+
+with open("output.txt", "w") as outputFile: 
+    for x in outputList:
+        outputFile.write("%s\n" % x)
+
+# outputFile = open("output.txt", "r")
+# print(outputFile.read())
+outputFile.close()
